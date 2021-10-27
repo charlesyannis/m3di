3fa16f04394983340541cda6f410ba1341a305ea
Yannis CHARLES <42754350+charlesyannis@users.noreply.github.com>
2021-10-27 21:04:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+##########################
+# M3DI 
+# Autor : Yannis CHARLES
+##########################
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
+
+print("M3Di-Guru")
